716d6b30338f8ae813a296e4ada812b903862800
Syafiq Hadzir <syafiqhadzir@live.com.my>
2020-12-16 14:45:16 +0800

    Include MIT license

--- AboutForm.Designer.cs
+++ AboutForm.Designer.cs
@@ -32,17 +32,27 @@
             this.panelTop = new System.Windows.Forms.Panel();
             this.panelLogo = new System.Windows.Forms.Panel();
             this.panelDesc = new System.Windows.Forms.Panel();
+            this.lbl_Subtitle = new System.Windows.Forms.Label();
             this.labelVersion = new System.Windows.Forms.Label();
             this.labelTitle = new System.Windows.Forms.Label();
             this.panelDev = new System.Windows.Forms.Panel();
             this.linkProject = new System.Windows.Forms.LinkLabel();
-            this.labelWebsite = new System.Windows.Forms.Label();
-            this.panelBottom = new System.Windows.Forms.Panel();
-            this.lbl_Subtitle = new System.Windows.Forms.Label();
-            this.lnk_Version = new System.Windows.Forms.LinkLabel();
             this.lnk_License = new System.Windows.Forms.LinkLabel();
+            this.labelWebsite = new System.Windows.Forms.Label();
+            this.lnk_Version = new System.Windows.Forms.LinkLabel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.grpBxMIT = new System.Windows.Forms.GroupBox();
+            this.lbl_Desc3 = new System.Windows.Forms.Label();
+            this.lbl_Desc2 = new System.Windows.Forms.Label();
+            this.lbl_Desc = new System.Windows.Forms.Label();
+            this.lbl_Author = new System.Windows.Forms.Label();
+            this.lbl_Desc4 = new System.Windows.Forms.Label();
+            this.lbl_Desc5 = new System.Windows.Forms.Label();
+            this.lbl_Desc6 = new System.Windows.Forms.Label();
             this.panelDesc.SuspendLayout();
             this.panelDev.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.grpBxMIT.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelTop
@@ -52,7 +62,7 @@
             this.panelTop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.panelTop.Location = new System.Drawing.Point(0, 0);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(608, 99);
+            this.panelTop.Size = new System.Drawing.Size(608, 11);
             this.panelTop.TabIndex = 0;
             // 
             // panelLogo
@@ -61,7 +71,7 @@
             this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.panelLogo.Location = new System.Drawing.Point(0, 99);
+            this.panelLogo.Location = new System.Drawing.Point(0, 11);
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(608, 100);
             this.panelLogo.TabIndex = 0;
@@ -75,10 +85,20 @@
             this.panelDesc.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.panelDesc.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.panelDesc.Location = new System.Drawing.Point(0, 199);
+            this.panelDesc.Location = new System.Drawing.Point(0, 111);
             this.panelDesc.Name = "panelDesc";
             this.panelDesc.Size = new System.Drawing.Size(608, 100);
             this.panelDesc.TabIndex = 0;
+            // 
+            // lbl_Subtitle
+            // 
+            this.lbl_Subtitle.AutoSize = true;
+            this.lbl_Subtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Subtitle.Location = new System.Drawing.Point(138, 44);
+            this.lbl_Subtitle.Name = "lbl_Subtitle";
+            this.lbl_Subtitle.Size = new System.Drawing.Size(317, 20);
+            this.lbl_Subtitle.TabIndex = 0;
+            this.lbl_Subtitle.Text = "An Open Malayo-Polynesian Dictionary";
             // 
             // labelVersion
             // 
@@ -116,7 +136,7 @@
             this.panelDev.Controls.Add(this.lnk_Version);
             this.panelDev.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelDev.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.panelDev.Location = new System.Drawing.Point(0, 299);
+            this.panelDev.Location = new System.Drawing.Point(0, 211);
             this.panelDev.Name = "panelDev";
             this.panelDev.Size = new System.Drawing.Size(608, 100);
             this.panelDev.TabIndex = 0;
@@ -133,6 +153,18 @@
             this.linkProject.Text = "https://nativescribe.syafiqhadzir.dev/";
             this.linkProject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // lnk_License
+            // 
+            this.lnk_License.AutoSize = true;
+            this.lnk_License.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lnk_License.Location = new System.Drawing.Point(184, 23);
+            this.lnk_License.Name = "lnk_License";
+            this.lnk_License.Size = new System.Drawing.Size(224, 17);
+            this.lnk_License.TabIndex = 0;
+            this.lnk_License.TabStop = true;
+            this.lnk_License.Text = "License and Open Source Notices";
+            this.lnk_License.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // labelWebsite
             // 
             this.labelWebsite.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -145,26 +177,6 @@
             this.labelWebsite.TabIndex = 0;
             this.labelWebsite.Text = "Official Website";
             this.labelWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panelBottom
-            // 
-            this.panelBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelBottom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.panelBottom.Location = new System.Drawing.Point(0, 399);
-            this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(608, 100);
-            this.panelBottom.TabIndex = 0;
-            // 
-            // lbl_Subtitle
-            // 
-            this.lbl_Subtitle.AutoSize = true;
-            this.lbl_Subtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Subtitle.Location = new System.Drawing.Point(138, 44);
-            this.lbl_Subtitle.Name = "lbl_Subtitle";
-            this.lbl_Subtitle.Size = new System.Drawing.Size(317, 20);
-            this.lbl_Subtitle.TabIndex = 0;
-            this.lbl_Subtitle.Text = "An Open Malayo-Polynesian Dictionary";
             // 
             // lnk_Version
             // 
@@ -180,17 +192,99 @@
             this.lnk_Version.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lnk_Version.UseCompatibleTextRendering = true;
             // 
-            // lnk_License
+            // panelBottom
             // 
-            this.lnk_License.AutoSize = true;
-            this.lnk_License.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lnk_License.Location = new System.Drawing.Point(184, 23);
-            this.lnk_License.Name = "lnk_License";
-            this.lnk_License.Size = new System.Drawing.Size(224, 17);
-            this.lnk_License.TabIndex = 0;
-            this.lnk_License.TabStop = true;
-            this.lnk_License.Text = "License and Open Source Notices";
-            this.lnk_License.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.panelBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelBottom.Controls.Add(this.grpBxMIT);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBottom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.panelBottom.Location = new System.Drawing.Point(0, 311);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(608, 188);
+            this.panelBottom.TabIndex = 0;
+            // 
+            // grpBxMIT
+            // 
+            this.grpBxMIT.Controls.Add(this.lbl_Desc6);
+            this.grpBxMIT.Controls.Add(this.lbl_Desc5);
+            this.grpBxMIT.Controls.Add(this.lbl_Desc4);
+            this.grpBxMIT.Controls.Add(this.lbl_Desc3);
+            this.grpBxMIT.Controls.Add(this.lbl_Desc2);
+            this.grpBxMIT.Controls.Add(this.lbl_Desc);
+            this.grpBxMIT.Controls.Add(this.lbl_Author);
+            this.grpBxMIT.Location = new System.Drawing.Point(3, 6);
+            this.grpBxMIT.Name = "grpBxMIT";
+            this.grpBxMIT.Size = new System.Drawing.Size(602, 170);
+            this.grpBxMIT.TabIndex = 0;
+            this.grpBxMIT.TabStop = false;
+            this.grpBxMIT.Text = "MIT License";
+            // 
+            // lbl_Desc3
+            // 
+            this.lbl_Desc3.AutoSize = true;
+            this.lbl_Desc3.Location = new System.Drawing.Point(30, 89);
+            this.lbl_Desc3.Name = "lbl_Desc3";
+            this.lbl_Desc3.Size = new System.Drawing.Size(511, 17);
+            this.lbl_Desc3.TabIndex = 0;
+            this.lbl_Desc3.Text = "purpose and noninfringement. In no event shall the authors or copyright holders";
+            // 
+            // lbl_Desc2
+            // 
+            this.lbl_Desc2.AutoSize = true;
+            this.lbl_Desc2.Location = new System.Drawing.Point(30, 72);
+            this.lbl_Desc2.Name = "lbl_Desc2";
+            this.lbl_Desc2.Size = new System.Drawing.Size(525, 17);
+            this.lbl_Desc2.TabIndex = 0;
+            this.lbl_Desc2.Text = "including but not limited to the warranties of merchantability, fitness for a par" +
+    "ticular";
+            // 
+            // lbl_Desc
+            // 
+            this.lbl_Desc.AutoSize = true;
+            this.lbl_Desc.Location = new System.Drawing.Point(30, 55);
+            this.lbl_Desc.Name = "lbl_Desc";
+            this.lbl_Desc.Size = new System.Drawing.Size(515, 17);
+            this.lbl_Desc.TabIndex = 0;
+            this.lbl_Desc.Text = "The software is provided \"as is\", without warranty of any kind, express or implie" +
+    "d,";
+            // 
+            // lbl_Author
+            // 
+            this.lbl_Author.AutoSize = true;
+            this.lbl_Author.Location = new System.Drawing.Point(30, 28);
+            this.lbl_Author.Name = "lbl_Author";
+            this.lbl_Author.Size = new System.Drawing.Size(213, 17);
+            this.lbl_Author.TabIndex = 0;
+            this.lbl_Author.Text = "Copyright (c) 2020 Syafiq Hadzir";
+            // 
+            // lbl_Desc4
+            // 
+            this.lbl_Desc4.AutoSize = true;
+            this.lbl_Desc4.Location = new System.Drawing.Point(30, 106);
+            this.lbl_Desc4.Name = "lbl_Desc4";
+            this.lbl_Desc4.Size = new System.Drawing.Size(517, 17);
+            this.lbl_Desc4.TabIndex = 0;
+            this.lbl_Desc4.Text = "be liable for any claim, damages or other liability, whether in an actionof contr" +
+    "act,";
+            // 
+            // lbl_Desc5
+            // 
+            this.lbl_Desc5.AutoSize = true;
+            this.lbl_Desc5.Location = new System.Drawing.Point(30, 123);
+            this.lbl_Desc5.Name = "lbl_Desc5";
+            this.lbl_Desc5.Size = new System.Drawing.Size(518, 17);
+            this.lbl_Desc5.TabIndex = 0;
+            this.lbl_Desc5.Text = "tort or otherwise, arising from, out of or in connection with the software or the" +
+    " use";
+            // 
+            // lbl_Desc6
+            // 
+            this.lbl_Desc6.AutoSize = true;
+            this.lbl_Desc6.Location = new System.Drawing.Point(30, 140);
+            this.lbl_Desc6.Name = "lbl_Desc6";
+            this.lbl_Desc6.Size = new System.Drawing.Size(215, 17);
+            this.lbl_Desc6.TabIndex = 0;
+            this.lbl_Desc6.Text = "or other dealings in the software.";
             // 
             // AboutForm
             // 
@@ -215,6 +309,9 @@
             this.panelDesc.PerformLayout();
             this.panelDev.ResumeLayout(false);
             this.panelDev.PerformLayout();
+            this.panelBottom.ResumeLayout(false);
+            this.grpBxMIT.ResumeLayout(false);
+            this.grpBxMIT.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -233,5 +330,13 @@
         private System.Windows.Forms.Label lbl_Subtitle;
         private System.Windows.Forms.LinkLabel lnk_License;
         private System.Windows.Forms.LinkLabel lnk_Version;
+        private System.Windows.Forms.GroupBox grpBxMIT;
+        private System.Windows.Forms.Label lbl_Author;
+        private System.Windows.Forms.Label lbl_Desc2;
+        private System.Windows.Forms.Label lbl_Desc;
+        private System.Windows.Forms.Label lbl_Desc3;
+        private System.Windows.Forms.Label lbl_Desc6;
+        private System.Windows.Forms.Label lbl_Desc5;
+        private System.Windows.Forms.Label lbl_Desc4;
     }
 }
